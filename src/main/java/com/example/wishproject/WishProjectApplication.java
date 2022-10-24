39a4fefffe419f3bfcac48e5c4f2ebf7c24cabe9
Ella Frost <ded_19@hotmail.com>
2022-10-24 10:34:42 +0200

    First toto

---
+++ src/main/java/com/example/wishproject/WishProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wishproject/WishProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wishproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
